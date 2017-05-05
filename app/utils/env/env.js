3a7614aeb5697d8960c19546d6a2e04e043fb0ad
jsen <1922279340@qq.com>
2017-05-05 12:40:09 +0800

    change db to local

--- app/utils/env/env.js
+++ app/utils/env/env.js
@@ -3,7 +3,8 @@ let E = process.env;
 exports.ENVSET = function() {
 
 E.PORT = 3000;
-E.MongoUrl = 'mongodb://root:root@120.25.217.56:27017';
+// E.MongoUrl = 'mongodb://root:root@120.25.217.56:27017';
+E.MongoUrl = 'mongodb://127.0.0.1:27017';
 E.MongoTable = 'jrecord';
 E.RedisPort = 6379;
 E.RedisHost = '120.25.217.56';
